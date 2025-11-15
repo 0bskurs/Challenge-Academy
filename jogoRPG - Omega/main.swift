5cc963045e2640d8e1b4314482e3bf6f62e75283
Obskur <magnoart16@gmail.com>
2025-11-15 04:37:43 -0300

    .

--- jogoRPG - Omega/main.swift
+++ jogoRPG - Omega/main.swift
@@ -1,10 +1,3 @@
-//
-//  main.swift
-//  jogoRPG - Omega
-//
-//  Created by LUCAS MAGNO CAMPOS OLIVEIRA on 14/11/25.
-//
-
 import Foundation
 
 enum InventarioErros: Error{
@@ -12,63 +5,75 @@ enum InventarioErros: Error{
 }
 
 
-func Inventario(Lista: Array<String>) throws {
 
-    var listaInventario_String = [String]()
-    var listaInventario_Int = [Int]()
-    var listaInventario_temp = [String]()
-    var temp = 0
-    
-    for _ in 0..<Lista.count
-    {
-        listaInventario_temp = listaInventario[temp].components(separatedBy: ",")
-        listaInventario_String.append(listaInventario_temp[0])
-        listaInventario_Int.append(Int(listaInventario_temp[1]) ?? 0)
-        print("\(listaInventario_String[temp])")
-        temp += 1
-    }
-    print ("Moedas: ")
-    return moedas = Int(readLine() ?? "") ?? 0
-    
-}
-func Mercado(ListaMercado: Array<String>, ListaInventario: Array<String>) throws {
+func Mercado() throws{
+    let ListaMercado = ["Cajado do Abismo,3500", "Livro de Sangue,4000", "Pocao de Cura,50", "Pocao de Mana,100"]
+    var listaInventario = ["Machado da Floresta,1000", "Cajado das Montanhas,3000"]
     var temp = 0
     var opcao = 0
     var itemComprar = 0
     var itemVender = 0
+    var listaInventario_temp = [String]()
     var listaMercado_String = [String]()
     var listaMercado_Int = [Int]()
     var listaInventario_String = [String]()
     var listaInventario_Int = [Int]()
-    var listaInventario_temp = [String]()
-    print("**Feiticeiro ancião - Aelfrun** \n")
-    print("- Olá. Bem vindo/a a minha coleção. \n Hoje está em estoque:")
-    print("")
-    print("O que você gostaria de fazer?")
-    print("0 - Comprar um item.")
-    print("1 - Vender um item")
-    opcao = Int(readLine() ?? "") ?? 0
-    for _ in 0..<ListaInventario.count
+    var listaMercado_temp = [String]()
+    print("==> Entrando em: Torre Divina\n")
+    print("'Ola, sou Aelfrun. O indomavel feiticeiro anciao. Bem vindo/a a minha colecao. \n Hoje esta em estoque'")
+    for _ in 0..<ListaMercado.count
     {
-        listaInventario_temp = ListaInventario[temp].components(separatedBy: ",")
+        listaMercado_temp = ListaMercado[temp].components(separatedBy: ",")
+        listaMercado_String.append(listaMercado_temp[0])
+        listaMercado_Int.append(Int(listaMercado_temp[1]) ?? 0)
+        print("\(temp) - \(listaMercado_String[temp]) por \(listaMercado_Int[temp]) moedas.")
+        temp += 1
+    }
+    print("")
+    print("'O que voce gostaria de fazer?'")
+    print("0 - Comprar um item.")
+    print("1 - Vender um item\n")
+    opcao = Int(readLine() ?? "" ) ?? 0
+    temp = 0
+    for _ in 0..<listaInventario.count
+    {
+        listaInventario_temp = listaInventario[temp].components(separatedBy: ",")
         listaInventario_String.append(listaInventario_temp[0])
         listaInventario_Int.append(Int(listaInventario_temp[1]) ?? 0)
         temp += 1
     }
+    temp = 0
+    print ("\nMoedas: ")
+    var moedas = Int(readLine() ?? "") ?? 0
     if opcao == 0
     {
-        print("Qual item você gostaria de comprar?")
-        print("")
-        for _ in 0..<ListaMercado.count
-        {
-            print("\(temp) -> \(listaMercado_String[temp]) | \(listaMercado_Int[temp])")
-            temp += 1
-        }
+    
+        print("\n 'Qual item voce gostaria de comprar?'\n")
+        
+        temp = 0
         itemComprar = Int(readLine() ?? "") ?? 0
+        
+        temp = 0
+        
         if moedas >= listaMercado_Int[itemComprar]
         {
             listaInventario.append(ListaMercado[itemComprar])
-            print("Moedas: \(moedas)")
+            listaInventario_temp = listaInventario[listaInventario.count - 1].components(separatedBy: ",")
+            listaInventario_String.append(listaInventario_temp[0])
+            listaInventario_Int.append(Int(listaInventario_temp[1]) ?? 0)
+            moedas = moedas - listaMercado_Int[itemComprar]
+            
+            print("\n Moedas: \(moedas)")
+            print("\n Inventário: ")
+            temp = 0
+            for _ in 0..<listaInventario.count
+        {
+            print("\(listaInventario_String[temp]) | \(listaInventario_Int[temp]) moedas")
+            temp += 1
+            
+        }
+            temp = 0
+        
         }
         else
         {
@@ -78,35 +83,41 @@ func Mercado(ListaMercado: Array<String>, ListaInventario: Array<String>) throws
     }
     else if opcao == 1
     {
-        print("Qual item você gostaria de vender?")
+        print("Qual item voce gostaria de vender?")
         print("")
-        for _ in 0..<listaMercado.count
+        temp = 0
+        itemVender = Int(readLine() ?? "") ?? 0
+        listaInventario_temp = listaInventario[listaInventario.count].components(separatedBy: ",")
+        listaInventario_String.append(listaInventario_temp[0])
+        listaInventario_Int.append(Int(listaInventario_temp[1]) ?? 0)
+        for _ in 0..<listaInventario.count
         {
             print("\(temp) - \(listaInventario_String[temp]) | \(listaInventario_Int[temp])")
             temp += 1
+            
         }
-        itemVender = Int(readLine() ?? "") ?? 0
+        
+        
+        
         moedas += listaInventario_Int[itemVender]
         listaInventario.remove(at: itemVender)
-        print("Moedas: \(moedas)")
+        print("\n Moedas: \(moedas)")
+        print("\n Inventário: ")
+        for _ in 0..<listaInventario.count
+        {
+            print("\(listaInventario_String[temp]) | \(listaInventario_Int[temp]) moedas")
+            temp += 1
+        }
     }
 }
 
 var moedas = 0
-var listaMercado = ["Cajado do Abismo,3500", "Livro de Sangue,4000", "Pocao de Cura,50", "Pocao de Mana,100"]
 
-var listaInventario = ["Machado da Floresta,1000", "Cajado das Montanhas,3000"]
+
+
 do {
-    try Inventario(Lista: listaInventario)
-    try Mercado(ListaMercado: listaMercado, ListaInventario: listaInventario)
-    try Inventario(Lista: listaInventario)
+    try Mercado()
 }
 catch InventarioErros.saldoInsuficiente {
     print("Saldo insuficiente")
 }
-
-
-
-
-
-
